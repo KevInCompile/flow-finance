a355b9dbeadf3b23827ccdbb2a65aadc14a1bb00
Kevin Barobn <innovaciot99@gmail.com>
2024-04-30 08:59:11 -0500

    middleware create

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function middleware(req: NextResponse) {
-  try {
-    if (window.localStorage.token) return NextResponse.next();
-  } catch (err) {
-    return NextResponse.redirect(new URL("/forbbiden", req.url));
-  }
+  let cookie = cookies().get("next-auth.session-token");
+  const response = NextResponse.next();
+
+  if (!cookie) return NextResponse.redirect(new URL("/forbbiden", req.url));
+  return response;
 }
 
 export const config = { matcher: ["/app"] };
