7047e7921af7e40ddecf82200e1ab9f5b4d592a6
KevBT <kevcoc10@gmail.com>
2024-05-04 11:52:08 -0500

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
-import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function middleware(req: NextResponse) {
   let cookie = cookies().get("next-auth.session-token");
+  let cookieProd = cookies().get("__Secure-next-auth.session-token");
   const response = NextResponse.next();
 
-  if (!cookie) return NextResponse.redirect(new URL("/forbbiden", req.url));
+  if (!cookieProd) return NextResponse.redirect(new URL("/forbbiden", req.url));
   return response;
 }
 
