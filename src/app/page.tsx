0fd500c180897eaa7483ad0f2a8c11890c89ed02
KevBT <kevcoc10@gmail.com>
2024-08-01 20:58:59 -0500

    Se dejan solo los circulos de colores al inicio de la web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
   return (
     <>
       <Head />
+    <div className="relative h-full w-full bg-transparent"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(220,172,10,.25),rgba(255,255,255,0))]"></div><div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(218,186,255,.45),rgba(255,255,255,0))]"></div></div>
       <main className="!w-full animate-fade-in ">
         <div className="animate-fade-in-down container max-w-[800px] m-auto">
           <h1 className="text-center mb-16 [font-size:5.5rem] [line-height:1] font-bold">
