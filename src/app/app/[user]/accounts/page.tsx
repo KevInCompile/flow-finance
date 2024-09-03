37e8da1e171d7158af2361d32c443dfa8978fe27
KevBT <kevcoc10@gmail.com>
2024-09-03 16:02:54 -0500

    The add accounts button is made visible

--- src/app/app/[user]/accounts/page.tsx
+++ src/app/app/[user]/accounts/page.tsx
@@ -18,34 +18,25 @@ export default function Accounts() {
     <>
       <Head />
       <section>
-        <div className="flex gap-3 items-center max-w-[800px] m-auto justify-center">
-          {loading ? <div className="w-full"><SkeletonTitle /></div> : data.length >= 1 ? (
-            <div className="flex items-center justify-center pb-3">
-              <h3 className="text-center font-bold text-white text-xl md:text-2xl">
-                Tienes <span className='text-purple-300'>{data.length}</span> cuentas registradas.
-              </h3>
-              <OpenButton />
-            </div>
-          ) : (
-            <h3 className="text-center font-bold text-white text-2xl">
-              Ingresa tus cuentas de{" "}
-              <span className="text-purple-300">activos.</span>
-            </h3>
-          )}
+        <div className="flex items-center justify-center pb-3">
+         <h1 className='text-2xl font-medium text-center text-[var(--color-usage)]'>Cuentas</h1>
+         <OpenButton />
         </div>
+        <div className="flex gap-3 items-center max-w-[800px] m-auto justify-center">
           {
             loading ? <></> :
             data.length === 0
               ? (
-                <div className="flex flex-col gap-3 items-center m-auto w-full py-5">
+                <div className="flex flex-col gap-3 items-center m-auto w-full">
                   <Image src={Gift} alt='Sin items' className="rounded-md px-5 md:px-0" priority />
                   <small className="opacity-50 italic font-medium text-white">
-                    No tienes nada registrado aún!
+                    No tienes nada registrado aún! Ingresa tus cuentas de activos
                   </small>
                 </div>
                 )
             : <></>
           }
+        </div>
         <div className="grid grid-cols-1 px-10 gap-5 py-5 md:grid-cols-4">
           {loading ? <SkeletonAccount /> : <Card data={data} setData={setData} />}
         </div>
