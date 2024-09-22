64ae59f195ecd38d9d2a0a3998445e51593479d3
KevInCompile <kevcoc10@gmail.com>
2024-09-22 17:40:10 -0500

    Route app/private/[username] -> /private

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <LoaderPage />
       </div>
     )
-  if (status === 'authenticated') return router.push(`/app/private/profile`)
+  if (status === 'authenticated') return router.push(`/private/profile`)
 
   return (
     <>
