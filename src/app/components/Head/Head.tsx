3ea416a139de55cd836d159eca54eb7e09f90619
KevInCompile <kevcoc10@gmail.com>
2024-11-06 23:59:57 -0500

    m

--- src/app/components/Head/Head.tsx
+++ src/app/components/Head/Head.tsx
@@ -37,7 +37,7 @@ export default function Head() {
           </div>
         ) : (
           <div className="bg-[#DDDDDD] font-bold bg-blur rounded-xl p-2 px-5 text-black">
-            v 1.3.2
+            v 1.3.1
           </div>
         )}
       </div>
