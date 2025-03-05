8dad20e1e4aa572ae82a5df29c99a5079bd163f7
KevInCompile <kevcoc10@gmail.com>
2025-03-05 09:58:51 -0500

    V0.2.3 add:categories system:

--- src/app/components/Head/Head.tsx
+++ src/app/components/Head/Head.tsx
@@ -37,7 +37,7 @@ export default function Head() {
           </div>
         ) : (
           <div className="bg-[#DDDDDD] font-bold bg-blur rounded-xl p-2 px-5 text-black">
-            v0.2.2
+            v0.3.2
           </div>
         )}
       </div>
